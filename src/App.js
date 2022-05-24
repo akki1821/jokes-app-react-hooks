faa1e9e4ef17c4dbac4c9d114ab5a4354ada9388
akki1821 <pppatelakash@gmail.com>
2022-05-24 02:17:51 -0700

    change in file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState ,useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const[joke,setJoke]=useState("please wait....")
+  const newjoke = ()=>{
+    fetch("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe"
+    ).then(res=>res.json())
+    .then(res2=>{
+      console.log(res2)
+      setJoke(res2.value.joke)
+    })
+
+  }
+  useEffect(()=>{
+    
+newjoke()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >live,love, laugh with joke...</h1>
+      <h4>{joke}</h4>
+      <button className="head" onClick={()=>newjoke()}>Click for new one!</button>
+
+      
     </div>
   );
 }
