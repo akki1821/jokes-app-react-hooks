a0b4d24100a3a2e33314c4805259a7ac9bb01902
akki1821 <pppatelakash@gmail.com>
2022-05-24 02:26:52 -0700

    chaged in file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, {useState ,useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const[joke,setJoke]=useState("please wait....")
-  const newjoke = ()=>{
-    fetch("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe"
-    ).then(res=>res.json())
-    .then(res2=>{
-      console.log(res2)
-      setJoke(res2.value.joke)
-    })
-
-  }
-  useEffect(()=>{
-    
-newjoke()
-  },[])
+  const [joke, setJoke] = useState("please wait....");
+  const newjoke = () => {
+    fetch("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe")
+      .then((res) => res.json())
+      .then((res2) => {
+        console.log(res2);
+        setJoke(res2.value.joke);
+      });
+  };
+  useEffect(() => {
+    newjoke();
+  }, []);
   return (
     <div className="App">
-      <h1 >live,love, laugh with joke...</h1>
+      <h1>live,love, laugh with joke...</h1>
       <h4>{joke}</h4>
-      <button className="head" onClick={()=>newjoke()}>Click for new one!</button>
-
-      
+      <button className="head" onClick={() => newjoke()}>
+        Click for new one!
+      </button>
     </div>
   );
 }
